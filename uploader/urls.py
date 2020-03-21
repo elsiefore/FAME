48a378211e210d0577e269e5e1e64e6b68eca392
Hanyu Wang <hanyuw1993@gmail.com>
2020-03-21 15:44:49 +0800

    setup boto client

--- uploader/urls.py
+++ uploader/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-               path('', views.index, name='index'),
+               path('', views.home, name='home'),
                path('health', views.health, name='health'),
                ]
 
--- uploader/views.py
+++ uploader/views.py
@@ -1,16 +1,26 @@
-from __future__ import unicode_literals
 
-from django.http import JsonResponse
-from django.http import Http404
+from django.http import JsonResponse,Http404,HttpResponseRedirect
 from django.shortcuts import render
 
-from .models import Video
 from .forms import VideoForm
+from django.urls import reverse
+from uploader.ibm_client import IBMCOSClient
+from django.core.files.storage import FileSystemStorage
 
-
-def index(request):
-    return render(request, 'index.html')
-
+def home(request):
+    if request.method=="POST":
+        form = VideoForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+            uploaded_file_path = fs.path(filename)
+            cos_client = IBMCOSClient()
+            cos_client.upload(uploaded_file_path,filename)
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        form=VideoForm()
+    return render(request,'index.html',{'form':form})
 
 def health(request):
     state = {"status": "UP"}
@@ -20,6 +30,6 @@ def health(request):
 def showvideo(request):
     # form= VideoForm(request.POST or None, request.FILES or None)
     # if form.is_valid():
-    # form.save()    
-      
+    # form.save()
+
     return 'Upload Successful'
