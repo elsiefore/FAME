264debe65f404e088dd08829d426e9fa18dff24e
elsiefore <a0119428@u.nus.edu>
2020-03-23 22:33:40 +0800

    save job to db after uploading

--- uploader/views.py
+++ uploader/views.py
@@ -1,14 +1,16 @@
 
-from django.http import JsonResponse,Http404,HttpResponseRedirect
+from django.http import JsonResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import VideoForm
+from .models import Job, StatusChoice
 from django.urls import reverse
 from uploader.ibm_client import IBMCOSClient
 from django.core.files.storage import FileSystemStorage
 
+
 def home(request):
-    if request.method=="POST":
+    if request.method == "POST":
         form = VideoForm(request.POST, request.FILES)
         if form.is_valid():
             file = request.FILES['file']
@@ -16,11 +18,18 @@ def home(request):
             filename = fs.save(file.name, file)
             uploaded_file_path = fs.path(filename)
             cos_client = IBMCOSClient()
-            cos_client.upload(uploaded_file_path,filename)
+            try:
+                cos_client.upload(uploaded_file_path, filename)
+                Job.objects.create(
+                    display_name=filename, s3_obejct_key=uploaded_file_path, status=StatusChoice.Processing)
+            except:
+                Job.objects.create(
+                    display_name=filename, s3_obejct_key=uploaded_file_path, status=StatusChoice.Failed)
             return HttpResponseRedirect(reverse('home'))
     else:
-        form=VideoForm()
-    return render(request,'index.html',{'form':form})
+        form = VideoForm()
+    return render(request, 'index.html', {'form': form})
+
 
 def health(request):
     state = {"status": "UP"}
