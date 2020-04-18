fdc35e219387dd880052b971736bbc0828410f51
elsiefore <a0119428@u.nus.edu>
2020-04-18 16:30:26 +0800

    add detail and overview graph

--- history/views.py
+++ history/views.py
@@ -11,5 +11,12 @@ def list(request):
 
 
 def job_detail(request, job_id):
+    job = Job.objects.get(id=job_id)
+
+    # below are for dev only
+    json_result = {"result": [{"timestamp": 1, "expressions": {"ANGRY": 1, "FEAR": 1, "SURPRISE": 2}}, {
+        "timestamp": 2, "expressions": {"ANGRY": 2, "FEAR": 1, "SURPRISE": 1}}]}
+    Result.objects.update(
+        job_id=job, result=json_result)
     result = Result.objects.get(job_id=job_id)
     return render(request, 'detail.html', {'result': result})
